fbb89f24ad61cac23a2f4ee7bad31ba7bf0ddec5
ckim48 <ckim48@u.rochester.edu>
2024-03-23 10:01:41 +0900

    sss

--- app.py
+++ app.py
@@ -18,5 +18,9 @@ def login():
     else: # method == "GET"
         return render_template('login.html')
 
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
